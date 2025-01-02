0f911004ffb1ee543b60ed1f37beda02b00ae63b
iacobdariaroxana <iacobdariaroxana@gmail.com>
2025-01-02 14:14:13 +0200

    add trip view model

---
+++ AdventurePlannerBE/ViewModels/TripDTO.cs
@@ -0,0 +1,31 @@
+﻿using AdventurePlannerBE.Models;
+using System.ComponentModel.DataAnnotations;
+
+namespace AdventurePlannerBE.ViewModels
+{
+    public class TripDTO
+    {
+        public Guid Id { get; set; }
+        public string Name { get; set; }
+
+        public DateOnly StartDate { get; set; }
+
+        public DateOnly EndDate { get; set; }
+
+        public int NumberOfPersons { get; set; }
+
+        public int EstimatedBudget { get; set; }
+
+        public TripDTO MapData(Trip trip)
+        {
+            Id = trip.Id;
+            Name = trip.Name;
+            StartDate = trip.StartDate;
+            EndDate = trip.EndDate;
+            NumberOfPersons = trip.NumberOfPersons;
+            EstimatedBudget = trip.EstimatedBudget;
+
+            return this;
+        }
+    }
+}

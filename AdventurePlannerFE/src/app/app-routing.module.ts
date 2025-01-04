298ac81469b9393cc914ac4e1614dc02d20c5928
iacobdariaroxana <iacobdariaroxana@gmail.com>
2025-01-04 23:27:47 +0200

    Add main and home page design

--- AdventurePlannerFE/src/app/app-routing.module.ts
+++ AdventurePlannerFE/src/app/app-routing.module.ts
@@ -1,10 +1,21 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomePageComponent } from './pages/home-page/home-page.component';
+import { AboutUsPageComponent } from './pages/about-us-page/about-us-page.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'home',
+    component: HomePageComponent,
+  },
+  {
+    path: 'aboutus',
+    component: AboutUsPageComponent,
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
--- AdventurePlannerFE/src/app/app.module.ts
+++ AdventurePlannerFE/src/app/app.module.ts
@@ -4,14 +4,34 @@ import { HttpClientModule } from '@angular/common/http';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { AppConfigService } from './services/app-config/app-config.service';
+import { HomePageComponent } from './pages/home-page/home-page.component';
+import { AppHeaderComponent } from './pages/app-header/app-header.component';
+import { AboutUsPageComponent } from './pages/about-us-page/about-us-page.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatButtonModule } from '@angular/material/button';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatIconModule } from '@angular/material/icon';
 
 export function appConfigFactory(appConfigService: AppConfigService) {
   return () => appConfigService.loadConfig();
 }
 
 @NgModule({
-  declarations: [AppComponent],
-  imports: [BrowserModule, AppRoutingModule, HttpClientModule],
+  declarations: [
+    AppComponent,
+    HomePageComponent,
+    AppHeaderComponent,
+    AboutUsPageComponent,
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    HttpClientModule,
+    BrowserAnimationsModule,
+    MatButtonModule,
+    MatToolbarModule,
+    MatIconModule,
+  ],
   providers: [
     AppConfigService,
     {

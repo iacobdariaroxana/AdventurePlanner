322dcda8d90db80444129be4ec0aaa198b4b7146
iacobdariaroxana <iacobdariaroxana@gmail.com>
2025-01-06 18:36:42 +0200

    clean-up

--- AdventurePlannerFE/src/app/app.component.ts
+++ AdventurePlannerFE/src/app/app.component.ts
@@ -1,6 +1,4 @@
 import { Component, OnInit } from '@angular/core';
-import { TripService } from './services/trip/trip.service';
-import { TripDto } from './dtos/trip-dto';
 
 @Component({
   selector: 'app-root',
@@ -10,19 +8,7 @@ import { TripDto } from './dtos/trip-dto';
 export class AppComponent implements OnInit {
   title = 'AdventurePlannerFE';
 
-  constructor(private tripService: TripService) {}
+  constructor() {}
 
-  ngOnInit(): void {
-    // this.tripService.deleteTrip('d7bb2374-6e4a-4750-ba8f-cceba8cc8fbe').subscribe((result) => {
-    //   console.log(result);
-    // })
-    // let trip = new TripDto();
-    // trip.name = 'paris';
-    // trip.startDate = '2025-01-04';
-    // trip.endDate = '2025-01-04';
-    // this.tripService.insertTrip(trip).subscribe({
-    //   next: (value) => console.log(value),
-    //   error: (err) => console.error(err),
-    // });
-  }
+  ngOnInit(): void {}
 }
